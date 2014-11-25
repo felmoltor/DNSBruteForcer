630eb7ca834e5faa4421e809cabe90694edc743d
Felipe Molina <felmoltor@gmail.com>
2014-11-25 17:53:37 +0100

    Add bundler

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "colorize"
+gem "nokogiri", "~>1.5.5"
--- dnsbrute.rb
+++ dnsbrute.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
+require 'bundler/setup'
+
 require 'pp'
 require 'optparse'
 require 'colorize'
